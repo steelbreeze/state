a7e10d8783aa71591714fda4ea0535f010dd41d9
Mesmo <mesmo@steelbreeze.net>
2018-11-06 16:55:02 +0000

    Last few comments

--- lib/node/util/random.d.ts
+++ lib/node/util/random.d.ts
@@ -1,4 +1,14 @@
 export declare namespace random {
+    /**
+     * Returns a random number between 0 and max - 1
+     * @param max The ceiling for the random number
+     * @returns Returns a random number between 0 and max - 1
+     */
     let get: (max: number) => number;
+    /**
+     * Overrides the default method of random number generation.
+     * @param value The new function to use to generate random numbers
+     * @returns Returns the old method used to generate random numbers
+     */
     function set(value: (max: number) => number): (max: number) => number;
 }
--- src/util/random.ts
+++ src/util/random.ts
@@ -1,6 +1,16 @@
 export namespace random {
+	/**
+	 * Returns a random number between 0 and max - 1
+	 * @param max The ceiling for the random number
+	 * @returns Returns a random number between 0 and max - 1
+	 */
 	export let get: (max: number) => number = (max: number) => Math.floor(Math.random() * max);
 
+	/**
+	 * Overrides the default method of random number generation.
+	 * @param value The new function to use to generate random numbers
+	 * @returns Returns the old method used to generate random numbers
+	 */
 	export function set(value: (max: number) => number): (max: number) => number {
 		const previous = get;
 
