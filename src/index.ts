ba42f7b0cfab4f851bbe6e4b4484f12f685de9b9
Mesmo <mesmo@steelbreeze.net>
2018-11-08 09:43:40 +0000

    Add active state configuration JSON interfaces to the public API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { PseudoStateKind, State, Region, PseudoState, ExternalTransition, InternalTransition, LocalTransition } from './model';
 
-export { IInstance, Instance } from './runtime';
+export { IInstance, IRegion, IState, Instance } from './runtime';
 
 export { log, random } from './util';
